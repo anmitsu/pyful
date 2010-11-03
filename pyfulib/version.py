042d598c933c68ae71691cfbe15fce4610dea7aa
anmitsu <anmitsu.s@gmail.com>
2010-11-03 16:49:30 +0900

    version: 0.1.1

--- pyfulib/version.py
+++ pyfulib/version.py
@@ -16,4 +16,4 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
