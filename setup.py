9fe7e2abe9018befbbf84e7349efeed5686e483f
anmitsu <anmitsu.s@gmail.com>
2011-02-01 10:32:16 +0900

    Modify setup script.

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ from distutils.core import setup
 
 from pyful import __version__
 
-setup(name = "pyful",
+setup(name = "Pyful",
       version = __version__,
       description = "Python file management utility",
+      long_description = "Python file management utility. This application is CUI filer of the keyboard operation for Linux.",
       author = "anmitsu",
       author_email = "anmitsu.s@gmail.com",
+      url = "http://github.com/anmitsu/pyful",
       license = "GPL",
       packages = ["pyful", "pyful/completion"],
       scripts = ["bin/pyful"],
