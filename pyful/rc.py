fa9ba527a67eb897357d6ca14c0b76b9a2259d86
anmitsu <anmitsu.s@gmail.com>
2011-03-22 15:39:43 +0900

    rc: add item of tiletop, tilebottom layout

--- pyful/rc.py
+++ pyful/rc.py
@@ -54,6 +54,8 @@ Workspace.default_path = '~/'
 #
 #     'Tile'
 #     'TileLeft'
+#     'TileTop'
+#     'TileBottom'
 #     'Oneline'
 #     'Onecolumn'
 #     'Magnifier'
@@ -424,6 +426,8 @@ menu.items['filer'] = (
 menu.items['layout'] = (
     ('(t)ile'        , KEY_t, commands['layout_tile']),
     ('tile(L)eft'    , KEY_L, commands['layout_tileleft']),
+    ('tile(T)op'     , KEY_T, commands['layout_tiletop']),
+    ('tile(B)ottom'  , KEY_B, commands['layout_tilebottom']),
     ('one(l)ine'     , KEY_l, commands['layout_oneline']),
     ('one(c)olumn'   , KEY_c, commands['layout_onecolumn']),
     ('(m)agnifier'   , KEY_m, commands['layout_magnifier']),
